3f35abf19445b3d48e20399d0010283621bb3cd2
Lucas Minter <lsminteris@gmail.com>
2022-01-19 16:47:58 -0800

    counter button test

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 import { Client, Intents } from "discord.js"
-import { ReacordDiscordJs, Button } from "reacord"
+import { ReacordDiscordJs, Embed, Button } from "reacord"
 import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const reacord = new ReacordDiscordJs(client)
 const channelId = process.env.CHANNEL_ID
+function Counter() {
+  const [count, setCount] = React.useState(0)
+  return (
+    <>
+      <Embed title="Counter">
+        This button has been clicked {count} times.
+      </Embed>
+      <Button onClick={() => setCount(count + 1)}>
+        +1
+      </Button>
+    </>
+  )
+}
 
 client.on("ready", () => {
-  client.application?.commands.create({
-    name: "ping",
-    description: "pong!",
-  })
+  reacord.send(channelId, <Counter />)
 })
 
-client.on("interactionCreate", (interaction) => {
-  if (interaction.isCommand() && interaction.commandName == "ping") {
-    // U=se the reply() function instead of send
-    reacord.reply(interaction, 'pong!')
-  }
-})
 await client.login(process.env.BOT_TOKEN)
