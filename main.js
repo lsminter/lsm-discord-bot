4040c85dcfbe58d3d680aadfa4bdc1f72b2c3269
Lucas Minter <lsminteris@gmail.com>
2022-03-11 15:42:22 -0800

    revert to working commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Client, Intents, Message, MessageEmbed } from "discord.js"
+import { Client, Intents } from "discord.js"
 import fetch from "node-fetch";
 import dotenv from 'dotenv'
 dotenv.config()
@@ -55,7 +55,8 @@ client.on("messageCreate", async message => {
 
     const minutes = userTimeResponse
     const currentTime = Math.round((new Date()).getTime() / 1000)
-    const remainingTime = `<t:${currentTime + (minutes * 60)}:R>`
+    const remainingTime = currentTime + (minutes * 60)
+
     const deleteTimer = (minutes * 60000) + 60000
 
     const embedEvent = {
@@ -64,8 +65,8 @@ client.on("messageCreate", async message => {
       description: `${message.author} is looking for a group to do ${userEventResponse}. Reply to this message with 👆 if you are interested.`,
       fields: [
         {
-          name: 'Time till event', 
-          value: remainingTime,
+          name: 'Time of event', 
+          value: `<t:${remainingTime}>`,
           inline: true
         },
         {
@@ -83,7 +84,7 @@ client.on("messageCreate", async message => {
     
     const embedMessage = await message.reply({ embeds: [embedEvent]})
     embedMessage.react('👆')
-    
+
     message.delete()
     botEventMessage.delete()
     botNumberMessage.delete()
@@ -110,6 +111,7 @@ client.on("messageCreate", async message => {
       embedMessage.delete()
     }, deleteTimer) 
   }
+
   else if (command === "eventstats" ){
 
     const calculatingMessage = await message.reply({content: "Calculating... Please be patient"})
